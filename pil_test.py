d7cdc9fd1d132482a6f6471376f10aa887d99833
koki <sikibuton@outlook.com>
2013-08-27 00:27:24 +0900

    pil_test

---
+++ pil_test.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      koki
+#
+# Created:     27/08/2013
+# Copyright:   (c) koki 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+     import Image
+     import ImageDraw
+     img = Image.new("RGB",(256,256),(255,0,0))
+     dr = ImageDraw.Draw(img)
+     dr.ellipse(((96,96),(160,160)),outline=(0,0,0) ,fill= (0,0,255))
+
+if __name__ == '__main__':
+    main()
